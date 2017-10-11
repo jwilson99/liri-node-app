2aec68f5f7a2721e0e1637f7cf9025c176b1ab99
jwilson99 <juliewilsongt@gmail.com>
2017-10-11 19:34:47 -0400

    created twitter api keys, added random.txt and keys.js (to contain the twitter data)

--- assets/js/keys.js
+++ assets/js/keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-    consumer_key: '<input here>',
-    consumer_secret: '<input here>',
-    access_token_key: '<input here>',
-    access_token_secret: '<input here>',
+    consumer_key: 'kiIeEBbw9S59jGJ7XHUV1hZL1',
+    consumer_secret: 'KPIFa0POmrzRShfCA9QGRJyJ9Qz6eGyRO4bgv9Bcv4GuVgN7b8',
+    access_token_key: '918209208469872645-8FZUx8PMN3kKH8aQGEDbEcmlKG3b98V',
+    access_token_secret: 'eFw9GOZgMqaOZN3jhfxhQXhwm8TXCG0XaMAg8HOAOhbhe',
 }
 
 module.exports = twitterKeys;
